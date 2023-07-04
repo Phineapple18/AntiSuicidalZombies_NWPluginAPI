9cba3cbf16433f75f7332f2eb64528fcf97307c2
AidualK <78623655+CarverGoofballJunior@users.noreply.github.com>
2023-07-04 12:46:01 +0200

    Update EventHandlers.cs

--- AntiSuicidalZombies/AntiSuicidalZombies/EventHandlers.cs
+++ AntiSuicidalZombies/AntiSuicidalZombies/EventHandlers.cs
@@ -1,50 +1,58 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using CustomPlayerEffects;
+using Interactables.Interobjects.DoorUtils;
+using MapGeneration;
+using PlayerRoles;
+using PlayerStatsSystem;
+using UnityEngine;
+
 using PluginAPI.Core.Attributes;
 using PluginAPI.Core;
 using PluginAPI.Enums;
-using CustomPlayerEffects;
-using Interactables.Interobjects.DoorUtils;
-using PlayerRoles;
-using PlayerStatsSystem;
-using MapGeneration;
-using UnityEngine;
+using PluginAPI.Events;
+using PluginAPI.Core.Zones;
 
 namespace AntiSuicidalZombies
 {
     public class EventHandlers
     {
 		[PluginEvent(ServerEventType.PlayerDamage)]
-		public bool OnPlayerDamage(Player player, Player attacker, DamageHandlerBase damageHandler)
+		public bool OnPlayerDamage(PlayerDamageEvent ev)
 		{
-			if (player.Role == RoleTypeId.Scp0492)
+			if (ev.Target.Role == RoleTypeId.Scp0492 && ev.Player == null)
 			{
-				UniversalDamageHandler uDH = (UniversalDamageHandler)damageHandler;
+				UniversalDamageHandler uDH = (UniversalDamageHandler)ev.DamageHandler;
 				if (uDH.TranslationId == 13)
 				{
 					if (config.BlindEffect > 0)
 					{
-						player.EffectsManager.EnableEffect<Blinded>(config.BlindEffect, true);
+						ev.Target.EffectsManager.EnableEffect<Blinded>(config.BlindEffect, true);
 					}
 					return false;
 				}
-				if (uDH.TranslationId == 20 && (player.Room.Name == RoomName.Hcz106 || player.Room.Name == RoomName.HczArmory || player.Room.Name == RoomName.HczTestroom))
+				if (uDH.TranslationId == 20 && (ev.Target.Room.Name == RoomName.Hcz106 || ev.Target.Room.Name == RoomName.HczArmory || ev.Target.Room.Name == RoomName.HczTestroom))
 				{
-					var roomdoors = DoorVariant.DoorsByRoom[player.Room].Where(d => d.RequiredPermissions.RequiredPermissions == KeycardPermissions.None).ToList();
+					var roomdoors = DoorVariant.DoorsByRoom[ev.Target.Room].Where(d => d.RequiredPermissions.RequiredPermissions == KeycardPermissions.None).ToList();
 					var doorpos = roomdoors.ElementAt(RandInt.Next(roomdoors.Count)).transform.position;
 					doorpos += Vector3.forward * 0.2f;
+					if (!RoomIdUtils.IsWithinRoomBoundaries(ev.Target.Room, doorpos))
+                    {
+						doorpos += Vector3.back * 0.2f;
+					}
 					doorpos += Vector3.up;
-					player.Position = doorpos;
+					ev.Target.Position = doorpos;
 					return false;
 				}
 			}
 			return true;
 		}
 
-		private static Config config = Plugin.Singleton.PluginConfig;
+		private static readonly Config config = Plugin.Singleton.PluginConfig;
 
 		private static readonly System.Random RandInt = new System.Random();
 	}
