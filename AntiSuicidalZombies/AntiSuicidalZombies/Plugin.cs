bc7bff9f0517c21e9f24908dcc08131340240054
AidualK <78623655+CarverGoofballJunior@users.noreply.github.com>
2023-09-03 13:22:10 +0200

    Update Plugin.cs

--- AntiSuicidalZombies/AntiSuicidalZombies/Plugin.cs
+++ AntiSuicidalZombies/AntiSuicidalZombies/Plugin.cs
@@ -15,7 +15,7 @@ namespace AntiSuicidalZombies
 		public static Plugin Singleton { get; private set; }
 
 		[PluginPriority(LoadPriority.Medium)]
-		[PluginEntryPoint("AntiSuicidalZombies", "1.0.2", null, "CarverGoofballJunior")]
+		[PluginEntryPoint("AntiSuicidalZombies", "1.0.4", null, "CarverGoofballJunior")]
 		public void OnLoad()
 		{
 			if (!PluginConfig.IsEnabled)
