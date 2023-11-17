29c55339c6d546169f8df84b623a6bbf7add6064
Phineapple18 <78623655+Phineapple18@users.noreply.github.com>
2023-11-17 23:32:02 +0100

    Update Plugin.cs

--- AntiSuicidalZombies/AntiSuicidalZombies/Plugin.cs
+++ AntiSuicidalZombies/AntiSuicidalZombies/Plugin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using PluginAPI.Core;
 using PluginAPI.Core.Attributes;
 using PluginAPI.Enums;
@@ -15,7 +16,7 @@ namespace AntiSuicidalZombies
 		public static Plugin Singleton { get; private set; }
 
 		[PluginPriority(LoadPriority.Medium)]
-		[PluginEntryPoint("AntiSuicidalZombies", "1.0.5", null, "CarverGoofballJunior")]
+		[PluginEntryPoint("AntiSuicidalZombies", "1.0.6", null, "phineapple_18")]
 		public void OnLoad()
 		{
 			if (!PluginConfig.IsEnabled)
