0aeb8ee8bdbf0bf709bb8963cf6341e445b87a60
Phineapple18 <78623655+Phineapple18@users.noreply.github.com>
2023-11-21 23:38:33 +0100

    Update EventHandlers.cs

--- AntiSuicidalZombies/AntiSuicidalZombies/EventHandlers.cs
+++ AntiSuicidalZombies/AntiSuicidalZombies/EventHandlers.cs
@@ -27,21 +27,21 @@ namespace AntiSuicidalZombies
 				if (udh.TranslationId == DeathTranslations.Tesla.Id && !config.TeslaEffect.IsEmpty())
 				{
 					foreach (var effect in config.TeslaEffect)
-				    	{
+					{
 						if (ev.Target.ReferenceHub.playerEffectsController.TryGetEffect(effect.Key, out StatusEffectBase effectBase) && effect.Value > 0)
-			                        {
-			                            effectBase.ServerSetState(1, effect.Value);
-			                        }
-				    	}
+						{
+							effectBase.ServerSetState(1, effect.Value);
+						}
+					}
 					return false;
 				}
 				if (udh.TranslationId == DeathTranslations.Crushed.Id && (ev.Target.Room.Name is RoomName.Hcz106 or RoomName.HczArmory or RoomName.HczTestroom))
 				{
-                   			var doors = DoorVariant.DoorsByRoom[ev.Target.Room].Where(d => d.RequiredPermissions.RequiredPermissions == KeycardPermissions.None).ToList();
+					var doors = DoorVariant.DoorsByRoom[ev.Target.Room].Where(d => d.RequiredPermissions.RequiredPermissions == KeycardPermissions.None).ToList();
 					var door = doors.ElementAt(RandInt.Next(doors.Count)).transform;
 					var endpos = door.position + door.transform.forward.normalized + Vector3.up;
 					if (!RoomIdUtils.IsWithinRoomBoundaries(ev.Target.Room, endpos))
-				    	{
+					{
 						endpos -= 2*door.transform.forward.normalized;
 					}
 					ev.Target.Position = endpos;
